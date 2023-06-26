ed539fbb19f20e0d8bd9b3fb4e6dbba8ebe80578
Heitor Saqueto <heitorsaqueto@hotmail.com>
2023-06-26 20:39:39 -0300

    test

---
+++ main.py
@@ -0,0 +1,31 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+
+teddy = Turtle()
+teddy.shape("classic")
+turtle.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rgb = (r, g, b)
+    return rgb
+
+teddy.speed(0)
+degree = [0, 90, 180, 270]
+
+
+def draw_spiograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        teddy.color(random_color())
+        teddy.circle(100)
+        teddy.setheading(teddy.heading() + size_of_gap)
+
+draw_spiograph(10)
+
+
+screen = Screen()
+screen.exitonclick()
